4079bdcff33331f6867b35fda867c46af6a90456
himham-jak <109818991+himham-jak@users.noreply.github.com>
2025-03-18 17:01:43 -0400

    extract env vars
    console logging to verify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+// import .env
+require('dotenv').config();
 function greeting(name) {
     console.log("Hello ".concat(name));
 }
 greeting('You');
+console.log(process.env.BSKY_USERNAME);
+console.log(process.env.BSKY_PASSWORD);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+// import .env
+require('dotenv').config()
+
 function greeting(name:string){
 	console.log(`Hello ${name}`);
 }
 
 greeting('You');
+
+console.log(process.env.BSKY_USERNAME)
+console.log(process.env.BSKY_PASSWORD)
